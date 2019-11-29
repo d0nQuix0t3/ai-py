7193dc5557c856f2ec9c648d0857a92538d2b1c3
Ryan Moos <mbpro@Ryans-MBP.hsd1.co.comcast.net>
2019-11-28 18:56:19 -0700

    AI Package - Framework

---
+++ ai/Test.py
@@ -0,0 +1,6 @@
+import os
+
+class PreProccessing:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
--- ai/__init__.py
+++ ai/__init__.py
@@ -1,4 +1,5 @@
-print("TEST")
+
 # Inside of __init__.py
+from . import Test
 #from .Text.PreProcessing import *
 #from ai.Model import *
